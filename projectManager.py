bb9251e07782eb9c2a514609707f6c721c1e8f80
ericandrsson <eric.andrsson@gmail.com>
2017-07-04 08:35:26 +1200

    fixes

--- projectManager.py
+++ projectManager.py
@@ -8,15 +8,20 @@ class ProjectManager:
 
     # Fetches and reads the assets csv file.
     assets_csv_url = 'https://docs.google.com/spreadsheets/d/1-a2K2BXe1uCPinImkMx5qVsuJcKoylxrWBqQxNl2yA0/pub?gid=797696484&single=true&output=csv'
-    req_assets_csv = requests.get(assets_csv_url, verify='C:/apps/python/2.7.8/Lib/site-packages/certifi/cacert.pem')
+    #req_assets_csv = requests.get(assets_csv_url, verify='C:/apps/python/2.7.8/Lib/site-packages/certifi/cacert.pem')
+    req_assets_csv = requests.get(assets_csv_url, timeout=10)
     assets_text = req_assets_csv.iter_lines()
 
     shots_csv_url= 'https://docs.google.com/spreadsheets/d/1-a2K2BXe1uCPinImkMx5qVsuJcKoylxrWBqQxNl2yA0/pub?gid=1343957549&single=true&output=csv'
-    req_shots_csv = requests.get(shots_csv_url, verify='C:/apps/python/2.7.8/Lib/site-packages/certifi/cacert.pem')
+    #req_shots_csv = requests.get(shots_csv_url, verify='C:/apps/python/2.7.8/Lib/site-packages/certifi/cacert.pem')
+    req_shots_csv = requests.get(shots_csv_url, timeout=10)
     shots_text = req_shots_csv.iter_lines()
 
+
+
     # Path to project folder.
-    projectFolder = 'C:/apps/autodesk/2017/Maya2017/plug-ins/camd/scripts/teamProduction/'
+    #projectFolder = 'C:/apps/autodesk/2017/Maya2017/plug-ins/camd/scripts/teamProduction/'
+    projectFolder = '/Users/EricAndersson/Documents/Projects/Scripting/Python/Maya/teamProduction/'
     # Path to project assets folder.
     assetFolder = os.path.join(projectFolder, 'assets')
 
@@ -32,11 +37,15 @@ class ProjectManager:
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
+        self.readAssets = csv.reader(self.assets_text, delimiter=',')
+        next(self.readAssets, None)
+        self.readShots = csv.reader(self.shots_text, delimiter=',')
+        next(self.readShots, None)
+
     def getAssignedAssets(self):
-        readAssets = csv.reader(self.assets_text, delimiter=',')
         self.assignedAssets = collections.namedtuple('Assets', ['Name', 'Type', 'Assigned_To', 'Status', 'Task', 'Bid', 'BidPercent'])
         assetList = []
-        for row in readAssets:
+        for row in self.readAssets:
             for field in row:
                 if field == self.user:
                     self.asset = self.assignedAssets(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
@@ -46,19 +55,15 @@ class ProjectManager:
         return assetList
 
 
-    def getAssignedShots(self):
-        pass
-        '''readShots = csv.reader(self.shots_text, delimiter=',')
-        self.assignedShots = collections.namedtuple('Shots', ['Shot', 'Status', 'Assigned_To', 'Bid', 'BidPercent'])
+    def getShots(self):
+        self.assignedShots = collections.namedtuple('Shots', ['Shot', 'Step', 'Status', 'Assigned_To', 'Bid', 'BidPercent'])
         shotList = []
-        for row in readShots:
-            for field in row:
-                if field == self.user:
-                    self.shot = self.assignedShots(row[0], row[1], row[2], row[5], row[6])
-                    assignedShots.append(row[0])
+        for row in self.readShots:
+            self.shot = self.assignedShots(row[0], row[1], row[2], row[3], row[4], row[5])
+            shotList.append(self.shot)
 
-        return assignedShots
-        '''
+        return shotList
+
     def newMayaProj(self, path, task_name, task_type):
         for f in os.listdir(path):
             if f.endswith('.ma'):
--- projectManagerUI.py
+++ projectManagerUI.py
@@ -24,17 +24,19 @@ class ProjectManagerUI(QtWidgets.QDialog):
 
         # Get UI Elements
         self.my_tasks_list = self.ui.findChild(QtWidgets.QListWidget, 'my_tasks_listWidget')
-        self.my_tasks_files = self.ui.findChild(QtWidgets.QListWidget, 'my_tasks_files_listWidget')
+        self.shots_list = self.ui.findChild(QtWidgets.QListWidget, 'shots_listWidget')
         self.new_file_btn = self.ui.findChild(QtWidgets.QPushButton, 'new_file')
         self.cancel_btn = self.ui.findChild(QtWidgets.QPushButton, 'cancel')
         self.open_file_btn = self.ui.findChild(QtWidgets.QPushButton, 'open_file')
         self.bid_time_bar = self.ui.findChild(QtWidgets.QProgressBar, 'bidtimeBar')
 
+        # Set default states
+        self.new_file_btn.setEnabled(False)
+        self.open_file_btn.setEnabled(False)
 
         # Create connections
         self.new_file_btn.clicked.connect(self.new_maya_proj)
         self.my_tasks_list.clicked.connect(self.getTaskInfo)
-        self.my_tasks_files.clicked.connect(self.get_file_version_selected)
         self.open_file_btn.clicked.connect(self.open_file)
         self.cancel_btn.clicked.connect(self.cancel)
 
@@ -46,7 +48,25 @@ class ProjectManagerUI(QtWidgets.QDialog):
     def populateMyTasks(self):
         self.assignedAssets = self.projectManager.getAssignedAssets()
         for asset in self.assignedAssets:
-            self.my_tasks_list.addItem(str(asset.Name) + ' (' + str(asset.Task)+ ')')
+            self.asset_maya_path = os.path.join(self.projectManager.assetFolder, asset.Type, asset.Name, asset.Task, 'work', 'maya')
+
+            if os.path.isfile(os.path.join(self.asset_maya_path, asset.Name + '_' + asset.Task + '_v001.ma')):
+                asset_versionList = []
+                # Searches for matching file and adds it to list. Sorts it and grabs the last file in the list (latest version)
+                for f in os.listdir(self.asset_maya_path):
+                    if f.startswith(str(asset.Name + '_' + asset.Task)) and f.endswith('.ma'):
+                        asset_versionList.append(f)
+
+                asset_versionList.sort()
+                try:
+                    asset_latest_version = asset_versionList[-1][-7:-3]
+                    self.my_tasks_list.addItem(str(asset.Name) + ' (' + str(asset.Task)+ ')' + ' - ' + asset_latest_version)
+                except:
+                    pass
+
+            else:
+                self.my_tasks_list.addItem(str(asset.Name) + ' (' + str(asset.Task)+ ')' + ' - No File')
+
 
         # If no folder for asset exists.
         prop_subFolders = ['art', 'model', 'rig', 'surface']
@@ -61,18 +81,13 @@ class ProjectManagerUI(QtWidgets.QDialog):
                         for prop_task_software_subFolder in prop_task_software_subFolders:
                             os.makedirs(os.path.join(assetDir, prop_subFolder, prop_task_subFolder, prop_task_software_subFolder))
 
-        #self.assignedShots = self.projectManager.getAssignedShots()
+        # Get shots and populate my_tasks and shots.
+        self.shots = self.projectManager.getShots()
+        for shot in self.shots:
+            self.shots_list.addItem(str(shot.Shot) + ' (' + str(shot.Step)+ ')')
+            if shot.Assigned_To == self.projectManager.user:
+                self.my_tasks_list.addItem(str(shot.Shot) + ' (' + str(shot.Step)+ ')')
 
-        '''
-        for asset in self.assignedAssets:
-            self.my_tasks_listWidget.addItem(asset)
-            self.assets_listWidget.addItem(asset)
-
-        self.assignedShots = self.projectManager.getAssignedShots()
-        for shot in self.assignedShots:
-            self.my_tasks_listWidget.addItem(shot)
-            self.shots_listWidget.addItem(shot)
-        '''
     def new_maya_proj(self):
         try:
             self.projectManager.newMayaProj(self.asset_maya_path, self.task_name, self.task_type)
@@ -82,13 +97,16 @@ class ProjectManagerUI(QtWidgets.QDialog):
 
 
     def getTaskInfo(self, item):
-        self.my_tasks_files.clear()
+        # Clears list to prevent stacking.
+        self.open_file_btn.setEnabled(False)
+        # Grabs the output from clicking.
         task = item.data()
+        # Splits the task into name + type and assigns it.
         self.task_name = task.split(' (')[0]
         self.task_type = task.split(' (')[1]
         self.task_type = self.task_type[:-1]
 
-        # Sets percentage
+        # Sets percentage of asset
         for asset in self.assignedAssets:
             if asset.Name == self.task_name:
                 if int(asset.BidPercent) > 100:
@@ -96,35 +114,52 @@ class ProjectManagerUI(QtWidgets.QDialog):
                 else:
                     self.bid_time_bar.setValue(int(asset.BidPercent))
 
+        for shot in self.shots:
+            if shot.Shot == self.task_name:
+                if int(shot.BidPercent) > 100:
+                    self.bid_time_bar.setValue(100)
+                else:
+                    self.bid_time_bar.setValue(int(shot.BidPercent))
+
         # Checks if file exists.
+
         for asset in self.assignedAssets:
             if asset.Name == self.task_name:
                 self.asset_maya_path = os.path.join(self.projectManager.assetFolder, asset.Type, asset.Name, asset.Task, 'work', 'maya')
+
+                # Checks if v001 file exists, enables/disables new_file button based on.
+                if not os.path.isfile(os.path.join(self.asset_maya_path, asset.Name + '_' + asset.Task + '_v001.ma')):
+                    self.new_file_btn.setEnabled(True)
+                else:
+                    self.new_file_btn.setEnabled(False)
+
+                asset_versionList = []
+                # Searches for matching file and adds it to list. Sorts it and grabs the last file in the list (latest version)
                 for f in os.listdir(self.asset_maya_path):
-                    if f.endswith('.ma'):
-                        self.my_tasks_files.addItem(f)
+                    if f.startswith(str(asset.Name + '_' + asset.Task)) and f.endswith('.ma'):
+                        asset_versionList.append(f)
+                asset_versionList.sort()
+                print self.my_tasks_files.addItem(asset_versionList[-1])
 
 
-    def get_file_version_selected(self, item):
-        self.file_version_selected = item.data()
 
     def open_file(self):
         try:
             if cmds.file(q=True, modified=True):
                 unsavedMsg = QtWidgets.QMessageBox()
                 unsavedMsg.setWindowTitle('Save your scene?')
-                unsavedMsg.setText('Your scene has unsaved changes. Save before procceding?') 
+                unsavedMsg.setText('Your scene has unsaved changes. Save before procceding?')
                 unsavedMsg.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Cancel)
                 unsavedMsg.setDefaultButton(QtWidgets.QMessageBox.Yes);
                 unsavedMsgRet = unsavedMsg.exec_()
 
                 if unsavedMsgRet == unsavedMsg.Yes:
-                    cmds.SaveScene()       
+                    cmds.SaveScene()
                     cmds.workspace(self.asset_maya_path, o=True)
                     cmds.file(os.path.join(self.asset_maya_path, self.file_version_selected), open=True)
                     self.close()
                 elif unsavedMsgRet == unsavedMsg.No:
-                    cmds.file(new=True, force=True)         
+                    cmds.file(new=True, force=True)
                     cmds.workspace(self.asset_maya_path, o=True)
                     cmds.file(os.path.join(self.asset_maya_path, self.file_version_selected), open=True)
                     self.close()
@@ -134,14 +169,11 @@ class ProjectManagerUI(QtWidgets.QDialog):
                 cmds.workspace(self.asset_maya_path, o=True)
                 cmds.file(os.path.join(self.asset_maya_path, self.file_version_selected), open=True)
                 self.close()
-                     
+
         except Exception as e: print(e)
 
     def cancel(self):
         self.close()
-
-    def msgbtn(self):
-        print('Canceling') 
 
 def showUI():
     ui = ProjectManagerUI()
